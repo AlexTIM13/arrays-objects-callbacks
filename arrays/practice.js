9a4c18ac880edd735e6e7dab708b7c57b3877bb5
AlexTIM13 <alejandro.partid@gmail.com>
2021-05-06 22:18:49 -0500

    primersave

--- arrays/practice.js
+++ arrays/practice.js
@@ -18,6 +18,11 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function  first (arr) {
+  return arr[0];
+}
+
+console.log(first(arr));
 
 
 
@@ -33,7 +38,11 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function  last (arr) {
+  return arr[2];
+}
 
+console.log(last(arr));
 
 
 ////////// PROBLEM 3 //////////
@@ -48,6 +57,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper (family) {
+  for (let i = 0; i < family.length; i++) {  
+  alert(family[i])
+  }
+}
 
 
 
@@ -63,6 +77,10 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper (letters) {
+for (let i = letters.length - 1; i >= 0; i--)
+alert(letters[i])
+}
 
 
 
